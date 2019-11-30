bc5b88bc418c1d4bd1dd72fda6c5dd8db124eedc
lizziegius <36507288+lizziegius@users.noreply.github.com>
2019-11-30 14:46:59 -0400

    Add files via upload

---
+++ Company.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gius
+{
+
+
+    public class Company
+    {
+
+        private string _name; private double _saveproject;
+        private string nombreini;
+
+        public string Name { get { return _name; } }
+        
+        public double SaveProject { get { return _saveproject; } }
+
+        public Company(string name, double saveproject) { _name = name; _saveproject = saveproject; }
+
+        public Company()
+        {
+        }
+
+        public Company(string nombreini)
+        {
+            this.nombreini = nombreini;
+        }
+
+        public void Save(double save) { _saveproject += save; }
+
+        public void Spend(double save) { _saveproject += save; }
+        public void ChangeName(string name) { _name = name; }
+
+    }
+
+}
+
+
---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gius;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompanyTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod] 
+        public void TestMethodNombre() 
+        {
+            //arreglo
+            string Nombreini = "Carla";
+            double Nombre
+            Company obj = new Company(Nombreini); 
+
+
+            
+            
+            //actuar
+            //hacer
+        }
+    }
+}
